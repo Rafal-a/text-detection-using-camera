4907fd45d0184666d2cdb0e6d8de5900d028a2a2
Rafal A <rafal.akeel99@gmail.com>
2022-08-15 14:42:10 +0300

    initial commit

---
+++ app/src/main/java/com/rafal/text_detection_using_camera/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.rafal.text_detection_using_camera
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
